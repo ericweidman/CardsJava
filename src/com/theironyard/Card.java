df73617b57f3c83622091419063088a84ec35269
ericweidman <ericweidman@gmail.com>
2016-03-28 12:17:08 -0400

    Added isFlush method

--- src/com/theironyard/Card.java
+++ src/com/theironyard/Card.java
@@ -28,4 +28,31 @@ public class Card {
         QUEEN,
         KING
     }
+
+    Suit suit;
+    Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (suit != card.suit) return false;
+        return rank == card.rank;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit != null ? suit.hashCode() : 0;
+        result = 31 * result + (rank != null ? rank.hashCode() : 0);
+        return result;
+    }
 }
