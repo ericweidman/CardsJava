0ef63e8189f6720ff9d556934bcf7f892aef2634
ericweidman <ericweidman@gmail.com>
2016-03-28 11:38:42 -0400

    Added Card Class

---
+++ src/com/theironyard/Card.java
@@ -0,0 +1,31 @@
+package com.theironyard;
+
+/**
+ * Created by ericweidman on 3/28/16.
+ */
+public class Card {
+
+
+    enum Suit{
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    enum Rank{
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+}
